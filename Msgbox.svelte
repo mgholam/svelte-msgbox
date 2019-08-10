504a5494a1fcb31ea5adf13b32a1b753182bea66
M. Gholam <mgholam@gmail.com>
2019-08-10 14:10:22 +0430

    - hsl theme color builder

--- Msgbox.svelte
+++ Msgbox.svelte
@@ -11,20 +11,24 @@
     // console.log(color);
     document.documentElement.style.setProperty(
       "--mb-colordark20",
-      LightenDarkenColor(color, -66)
+      // LightenDarkenColor(color, -66)
+      hexToHSL(color, -0.3)
     );
     document.documentElement.style.setProperty(
       "--mb-colordark10",
-      LightenDarkenColor(color, -33)
+      // LightenDarkenColor(color, -33)
+      hexToHSL(color, -0.1)
     );
     document.documentElement.style.setProperty("--mb-color", color);
     document.documentElement.style.setProperty(
       "--mb-colorlight10",
-      LightenDarkenColor(color, 33)
+      // LightenDarkenColor(color, 33)
+      hexToHSL(color, 0.1)
     );
     document.documentElement.style.setProperty(
       "--mb-colorlight20",
-      LightenDarkenColor(color, 66)
+      // LightenDarkenColor(color, 66)
+      hexToHSL(color, 0.3)
     );
   } else {
     document.documentElement.style.setProperty("--mb-colordark20", "#666");
@@ -173,35 +177,78 @@
     document.documentElement.removeEventListener("mouseup", stopDrag, false);
   }
 
-  function LightenDarkenColor(col, amt) {
-    var usePound = false;
-
-    if (col[0] == "#") {
-      col = col.slice(1);
-      usePound = true;
+  function hexToHSL(H, alpha) {
+    // Convert hex to RGB first
+    let r = 0,
+      g = 0,
+      b = 0;
+    if (H.length == 4) {
+      r = "0x" + H[1] + H[1];
+      g = "0x" + H[2] + H[2];
+      b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+      r = "0x" + H[1] + H[2];
+      g = "0x" + H[3] + H[4];
+      b = "0x" + H[5] + H[6];
     }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
 
-    amt = 1 + amt / 100;
+    if (delta == 0) h = 0;
+    else if (cmax == r) h = ((g - b) / delta) % 6;
+    else if (cmax == g) h = (b - r) / delta + 2;
+    else h = (r - g) / delta + 4;
 
-    var num = parseInt(col, 16);
+    h = Math.round(h * 60);
 
-    var r = (num >> 16) * amt;
+    if (h < 0) h += 360;
 
-    if (r > 255) r = 255;
-    else if (r < 0) r = 0;
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l += alpha;
+    l = +(l * 100).toFixed(1);
 
-    var b = ((num >> 8) & 0x00ff) * amt;
-
-    if (b > 255) b = 255;
-    else if (b < 0) b = 0;
-
-    var g = (num & 0x0000ff) * amt;
-
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-
-    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+    return "hsl(" + h + "," + s + "%," + l + "%)";
   }
+
+  // function LightenDarkenColor(col, amt) {
+  //   var usePound = false;
+
+  //   if (col[0] == "#") {
+  //     col = col.slice(1);
+  //     usePound = true;
+  //   }
+
+  //   amt = 1 + amt / 100;
+
+  //   var num = parseInt(col, 16);
+
+  //   var r = (num >> 16) * amt;
+
+  //   if (r > 255) r = 255;
+  //   else if (r < 0) r = 0;
+
+  //   var b = ((num >> 8) & 0x00ff) * amt;
+
+  //   if (b > 255) b = 255;
+  //   else if (b < 0) b = 0;
+
+  //   var g = (num & 0x0000ff) * amt;
+
+  //   if (g > 255) g = 255;
+  //   else if (g < 0) g = 0;
+
+  //   return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+  // }
 </script>
 
 <style>
@@ -324,7 +371,7 @@
       var(--mb-colorwhite),
       var(--mb-colorlight10)
     );
-    color: var(--mb-colorwhite);
+    /* color: var(--mb-colorwhite); */
     box-shadow: none;
   }
 </style>
